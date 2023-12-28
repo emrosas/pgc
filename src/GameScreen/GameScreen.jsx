36bee439ca34c1f31fa534c280620ab0c48d0f0e
Erick Mireles <emireles.rosas@gmail.com>
2023-12-28 16:24:52 -0600

    Fixed screen height scrolling issue and added overscroll behaviour none

--- src/GameScreen/GameScreen.jsx
+++ src/GameScreen/GameScreen.jsx
@@ -7,14 +7,33 @@ import PlayerCard from "../PlayerCard/PlayerCard";
 const GameScreen = ({ handleGameEnd }) => {
   return (
     <div className="game-screen-wrapper">
-      <Navbar handleGameEnd={handleGameEnd} />
       <main>
-        <ul>
-          <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
-          <PlayerCard name="Cletty" username="La mas Megafona" score="75" />
-          <PlayerCard name="Erick" username="Es bien TOP" score="50" />
-        </ul>
-        <section>This is a Section</section>
+        <div></div>
+        <Navbar handleGameEnd={handleGameEnd} />
+        <section>
+          <ul>
+            <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
+            <PlayerCard name="Cletty" username="La mas Megafona" score="75" />
+            <PlayerCard name="Erick" username="Es bien TOP" score="50" />
+          </ul>
+          <aside>
+            <div className="functionality-modal">
+              <h4>Functionality Modal</h4>
+              <p className="modal-info">This it where the magic happens!</p>
+              <button className="modal-btn">Do Nothing</button>
+            </div>
+            <div className="functionality-modal">
+              <h4>Functionality Modal</h4>
+              <p className="modal-info">This it where the magic happens!</p>
+              <button className="modal-btn">Do Nothing</button>
+            </div>
+            <div className="functionality-modal">
+              <h4>Functionality Modal</h4>
+              <p className="modal-info">This it where the magic happens!</p>
+              <button className="modal-btn">Do Nothing</button>
+            </div>
+          </aside>
+        </section>
       </main>
     </div>
   );
