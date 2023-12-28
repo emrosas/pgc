83f171b187a5a96605a9ed1f2f4595af578044f9
Erick Mireles <emireles.rosas@gmail.com>
2023-12-28 17:46:42 -0600

    Implemented add player functionality

--- src/GameScreen/GameScreen.jsx
+++ src/GameScreen/GameScreen.jsx
@@ -1,10 +1,16 @@
-import React from "react";
-import Logo from "../assets/logo.svg";
+import React, { useState } from "react";
 import "./GameScreen.css";
 import Navbar from "../Navbar/Navbar";
 import PlayerCard from "../PlayerCard/PlayerCard";
+import PlayerAddForm from "../PlayerAddForm/PlayerAddForm";
 
 const GameScreen = ({ handleGameEnd }) => {
+  const [players, setPlayers] = useState([]);
+
+  const handleAddPlayer = (newPlayer) => {
+    setPlayers([...players, newPlayer]);
+  };
+
   return (
     <div className="game-screen-wrapper">
       <main>
@@ -12,16 +18,22 @@ const GameScreen = ({ handleGameEnd }) => {
         <Navbar handleGameEnd={handleGameEnd} />
         <section>
           <ul>
-            <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
+            {players.map((player) => (
+              <PlayerCard
+                name={player.name}
+                username={player.username}
+                score={player.score}
+              />
+            ))}
+            {/* <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
             <PlayerCard name="Cletty" username="La mas Megafona" score="75" />
-            <PlayerCard name="Erick" username="Es bien TOP" score="50" />
-            <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
-            <PlayerCard name="Cletty" username="La mas Megafona" score="75" />
-            <PlayerCard name="Erick" username="Es bien TOP" score="50" />
-            <PlayerCard name="Jimmmy" username="Necesita un Baño" score="100" />
-            <PlayerCard name="Cletty" username="La mas Megafona" score="75" />
-            <PlayerCard name="Erick" username="Es bien TOP" score="50" />
+            <PlayerCard name="Erick" username="Es bien TOP" score="50" /> */}
+            <PlayerAddForm
+              handleAddPlayer={handleAddPlayer}
+              players={players}
+            />
           </ul>
+
           <aside>
             <div className="functionality-modal">
               <h4>Functionality Modal</h4>
