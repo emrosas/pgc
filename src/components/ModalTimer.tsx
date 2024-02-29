efdbb7ec8294d45f6d3c7f0b1340bf85a797b69a
Erick Mireles <emireles.rosas@gmail.com>
2024-02-28 22:12:06 -0600

    Add timer modal and fix timeout issue interfering with pause and restart functionalities

--- src/components/ModalTimer.tsx
+++ src/components/ModalTimer.tsx
@@ -1,49 +1,50 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import Start from "../assets/timer-start.svg";
 import Pause from "../assets/timer-pause.svg";
 import Restart from "../assets/timer-restart.svg";
 
 const ModalTimer = () => {
   const [timer, setTimer] = useState(0);
-  // const [lastTimer, setLastTimer] = useState(0);
-  // const intervalRef = useRef();
+  const [lastTimer, setLastTimer] = useState(0);
+  const intervalRef = useRef() as any;
 
-  // const handleChange = (e) => {
-  //   setTimer(e.target.value);
-  //   setLastTimer(e.target.value);
-  // };
-  // const startTimer = (e) => {
-  //   e.preventDefault();
-  //   if (intervalRef.current) return;
-  //   if (timer > 0) {
-  //     intervalRef.current = setInterval(() => {
-  //       setTimer((prevTimer) => prevTimer - 1);
-  //     }, 1000);
-  //     setTimeout(() => {
-  //       clearInterval(intervalRef.current);
-  //       intervalRef.current = null;
-  //     }, timer * 1000);
-  //   }
-  // };
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
-  // const pauseTimer = (e) => {
-  //   e.preventDefault();
-  //   clearInterval(intervalRef.current);
-  //   intervalRef.current = null;
-  // };
+  const startTimer = (e: any) => {
+    e.preventDefault();
+    if (intervalRef.current) return;
+    if (timer > 0) {
+      intervalRef.current = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+      timeoutRef.current = setTimeout(() => {
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
+      }, timer * 1000);
+    }
+  };
+  const handleSetTimer = (e: any) => {
+    setTimer(e.target.value);
+    setLastTimer(e.target.value);
+  };
 
-  // const restartTimer = (e) => {
-  //   e.preventDefault();
-  //   clearInterval(intervalRef.current);
-  //   intervalRef.current = null;
-  //   setTimer(lastTimer);
-  // };
+  const pauseTimer = (e: any) => {
+    e.preventDefault();
+    clearTimeout(timeoutRef.current as NodeJS.Timeout);
+    clearInterval(intervalRef.current);
+    intervalRef.current = 0;
+  };
+
+  const restartTimer = (e: any) => {
+    e.preventDefault();
+    clearTimeout(timeoutRef.current as NodeJS.Timeout);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setTimer(lastTimer);
+  };
 
   return (
-    <form
-      className="grid gap-4 grid-cols-2"
-      // onSubmit={startTimer}
-    >
+    <form className="grid gap-4 grid-cols-2" onSubmit={startTimer}>
       <div className="col-span-1 flex flex-col">
         <h4 className=" text-3xl">Timer</h4>
         <p className="text-sm">Enter a time in seconds</p>
@@ -53,14 +54,13 @@ const ModalTimer = () => {
           timer <= 3 && "text-red-600"
         }`}
         type="number"
-        // onChange={handleChange}
+        onChange={handleSetTimer}
         value={timer}
       />
       <div className="col-span-2 flex gap-4">
         <button
           className="px-4 py-2 bg-light-2 border border-dark-3 border-opacity-50 rounded-lg"
-          // onClick={pauseTimer}
-        >
+          onClick={pauseTimer}>
           <img src={Pause} alt="pause icon" />
         </button>
         <button
@@ -71,8 +71,7 @@ const ModalTimer = () => {
         </button>
         <button
           className="px-4 py-2 bg-light-2 border border-dark-3 border-opacity-50 rounded-lg"
-          // onClick={restartTimer}
-        >
+          onClick={restartTimer}>
           <img src={Restart} alt="restart icon" />
         </button>
       </div>
